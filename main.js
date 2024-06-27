3483950715c84579de8d3afe7532a121a19c7f2b
Olkobarylko <olegbarylko81@gmail.com>
2024-06-27 10:50:16 +0300

    added new frame

--- main.js
+++ main.js
@@ -1,68 +1,241 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   setTimeout(() => {
-    gsap.to(".bst-animation__left-border", {
+    let tl = gsap.timeline();
+
+    tl.to(".bst-animation__left-border", {
       duration: 2,
       opacity: 1,
       width: "140px",
       height: "214px",
     });
 
-    gsap.to(".mask1-img", {
+    tl.to(
+      ".mask1-img",
+      {
         duration: 2,
-        x:0,
-    });
+        x: 0,
+        y: "-90.6px",
+      },
+      "<"
+    );
 
-    gsap.to(".mask1", {
+    tl.to(
+      ".mask1",
+      {
         duration: 2,
         width: "214px",
         height: "237px",
-    });
+      },
+      "<"
+    );
 
-    gsap.to(".mask2-img", {
+    tl.to(
+      ".mask2-img",
+      {
         duration: 2,
-        x:"-210px",
-        y:"-105px"
-    });
+        x: "-235px",
+        y: "-92px",
+      },
+      "<"
+    );
 
-    gsap.to(".mask2", {
+    tl.to(
+      ".mask2",
+      {
         duration: 2,
         width: "211px",
         height: "243px",
-        y:"0"
-    });
+        y: "0",
+      },
+      "<"
+    );
 
-    gsap.to(".mask3-img", {
+    tl.to(
+      ".mask3-img",
+      {
         duration: 2,
-        x:"-412px",
-        y:"-105px"
-    });
+        x: "-463px",
+        y: "-83px",
+      },
+      "<"
+    );
 
-    gsap.to(".mask3", {
+    tl.to(
+      ".mask3",
+      {
         duration: 2,
         width: "212px",
         height: "46px",
-        x:"0"
-    });
+        x: "0",
+      },
+      "<"
+    );
 
-    gsap.to(".mask4-img", {
+    tl.to(
+      ".mask4-img",
+      {
         duration: 2,
-        x:"-475px",
-        y:"-180px"
-    });
+        x: "-534px",
+        y: "-160px",
+      },
+      "<"
+    );
 
-    gsap.to(".mask4", {
+    tl.to(
+      ".mask4",
+      {
         duration: 2,
         width: "55px",
         height: "170px",
-        x:"0"
-    });
+        x: "0",
+      },
+      "<"
+    );
 
-    gsap.to(".bst-animation__right-blur", {
+    tl.to(
+      ".bst-animation__right-blur",
+      {
         duration: 2,
         opacity: 1,
-        x:0,
-      });
+        x: 0,
+      },
+      "<"
+    );
 
+    tl.to(".bst-animation__main-image img", {
+      duration: 2,
+      width: "100%",
+      height: "100%",
+      borderTopRightRadius: "90px",
+    });
 
-  }, "1000");
+    tl.to(
+      ".bst-animation__left-border",
+      {
+        duration: 2,
+        x: "30px",
+        y: "-50px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__right-blur",
+      {
+        duration: 2,
+        y: "-70px",
+        x: "20px",
+      },
+      "<"
+    );
+
+    tl.to(".bst-animation__big-main-image", {
+      duration: 2,
+      opacity: 1,
+      scale: 1.292,
+    });
+
+    tl.to(
+      ".bst-animation__main-image",
+      {
+        duration: 2,
+        opacity: 0,
+        scale: 1.292,
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__right-blur",
+      {
+        duration: 2,
+        height: "222px",
+        width: "305px",
+        x: "230px",
+        y: "-120px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__left-border",
+      {
+        duration: 2,
+        width: "140px",
+        height: "301px",
+        x: "-80px",
+        y: "-120px",
+      },
+      "<"
+    );
+
+    tl.to(".bst-animation__container", {
+      duration: 2,
+      maxWidth: "1240px",
+    });
+
+    tl.to(
+      ".bst-animation__block",
+      {
+        duration: 2,
+        width: "545px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__big-main-image",
+      {
+        duration: 2,
+        scale: 1,
+        width: "100%",
+        borderTopRightRadius: "180px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__big-main-image img",
+      {
+        duration: 2,
+        borderTopRightRadius: "180px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__left-border",
+      {
+        duration: 2,
+        width: "96px",
+        height: "204px",
+        x: "20px",
+        y: "-60px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__right-blur",
+      {
+        duration: 2,
+        height: "121px",
+        width: "207px",
+        x: "100px",
+        y: "270px",
+      },
+      "<"
+    );
+
+    tl.to(
+      ".bst-animation__decor",
+      {
+        duration: 1,
+        opacity: 1,
+        x: "80px",
+        y: "-40px",
+      },
+      "<"
+    );
+  }, 1000);
 });
